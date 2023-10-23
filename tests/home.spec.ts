cf4450d403ab02bfa240d16382fa4f98436be4c0
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2023-10-23 20:48:11 +0200

    Setup first test

---
+++ tests/home.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Home', () => {
+    test('Open HomePage and verify title', async ({ page }) => {
+        // open url
+        await page.goto('https://practice.sdetunicorns.com/');
+
+        // verify title
+        await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns – Helping you succeed in Software Quality.')
+    })
+    
+})
