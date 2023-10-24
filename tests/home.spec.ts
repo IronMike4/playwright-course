696b55078522c9fa041b4acac73bbf4f8e28f727
Tinago Mahachi <tinago.mahachi@gviprograms.com>
2023-10-24 08:18:20 +0200

    Added a test for About page

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -9,4 +9,13 @@ test.describe('Home', () => {
         await expect(page).toHaveTitle('Practice E-Commerce Site – SDET Unicorns – Helping you succeed in Software Quality.')
     })
     
+    test('Open About page and verify title', async ({ page }) => {
+        // open url
+        await page.goto('https://practice.sdetunicorns.com/about/');
+
+        // verify title
+        await expect(page).toHaveTitle('About – Practice E-Commerce Site')
+        
+    })
+    
 })
